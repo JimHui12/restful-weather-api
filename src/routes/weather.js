197fefb2e25deb6900d52dbcede4a048206a802b
JimHui12 <jianghua.hui@uq.net.au>
2019-11-15 16:38:40 +1000

    gitignore added

--- src/routes/weather.js
+++ src/routes/weather.js
@@ -2,11 +2,12 @@ const express = require('express');
 const axios = require('axios');
 
 const router = express.Router();
+const APPID = process.env.APPID;
 
 router.get('/:cc/:city', (req, res) => {
     const {cc, city} = req.params;
     axios
-    .get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${cc}&APPID=3ad2909b326ee87242f4ce1c70aae3b0`
+    .get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${cc}&APPID=${APPID}`
     )
     .then(response => {
         res.send(response.data);
