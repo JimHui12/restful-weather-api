d2832313abbab67e9d112e8d59eb3fc5b521967c
JimHui12 <jianghua.hui@uq.net.au>
2019-11-15 17:08:44 +1000

    logger.js added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 require('dotenv').config();
 const routes = require('./routes');
-
-
+const helmet = require('helmet');
+const morgan = require('morgan');
+const logger = require('./utils/logger');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(helmet());
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+} else {
+    
+    app.use(morgan('common'));
+}
 
 app.use(routes);
 
