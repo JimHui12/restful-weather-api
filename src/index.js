405358d1b027dd1bdf8ddf35cf643c29cf6c3ece
JimHui12 <jianghua.hui@uq.net.au>
2019-11-17 10:38:31 +1000

    handle 404 errors function added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const routes = require('./routes');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const logger = require('./utils/logger');
+const notFoundHandler = require('./middlewares/notFound');
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,10 +14,11 @@ app.use(helmet());
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 } else {
-    
     app.use(morgan('common'));
 }
 
 app.use(routes);
+app.use(notFoundHandler);
+
 
 app.listen(PORT, () => console.log(`app listen on port ${PORT}`));
